edf5a3548966bbf410274438bcbfc2a6feb084b3
helderscrolls <nortehelder@gmail.com>
2019-04-27 17:50:39 +0200

    added logic to show/hide hero informations with ngFor and ngIf

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -9,12 +9,12 @@ import { HEROES } from '../../mocks/mock-heroes';
 })
 
 export class HeroesComponent {
-  
+
   heroes = HEROES;
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   };
 
 
