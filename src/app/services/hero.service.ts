0807bb62dac5b181fabd0c5a031f65ed6f084a37
helderscrolls <nortehelder@gmail.com>
2019-04-27 22:15:15 +0200

    created getHeroes function

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from '../classes/hero';
+import { HEROES } from '../mocks/mock-heroes';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  };
+  
 
   constructor() { }
 }
