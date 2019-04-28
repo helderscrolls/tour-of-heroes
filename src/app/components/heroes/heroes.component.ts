b4fcb0860813bff341a9cf0fcba60114de500541
helderscrolls <nortehelder@gmail.com>
2019-04-28 15:57:51 +0200

    added routes inside heroes of dashboard and heroes component. Also changed the SCSS of Heroes and Hero-Detail components

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -10,23 +10,16 @@ import { HeroService } from '../../services/hero.service';
 })
 
 export class HeroesComponent implements OnInit{
-
-  heroes: Hero[]; 
-
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
-
-
+  heroes: Hero[];
+  
   constructor(private heroService:HeroService) { }
-
+  
   ngOnInit() {
     this.getHeroes();
+  }
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
